4a0965a54df949412db24a6ef6c8add5006e152a
fangjim <jfang875@gmail.com>
2024-04-28 23:04:08 -0500

    frontend changes

--- frontend/src/hooks/useSpeechSynthesis.tsx
+++ frontend/src/hooks/useSpeechSynthesis.tsx
@@ -4,8 +4,29 @@ const useSpeechSynthesis = () => {
     const [speechSynthesisUtterance] = useState(new SpeechSynthesisUtterance());
 
     const speak = (text: string): void => {
-      speechSynthesisUtterance.text = text;
-      window.speechSynthesis.speak(speechSynthesisUtterance);
+        const messages = [
+            "A long-sleeved plaid shirt with dark blue, black, and light blue stripes",
+            "A long-sleeved blue plaid shirt with bold black and blue checkered pattern, with a button-down front and cuffs",
+            "A cozy, classic blue plaid shirt exudes a casual, yet stylish vibe, perfect for a crisp autumn day.", 
+            text
+        ];
+
+        const speakMessages = (index: number) => {
+            if (index >= messages.length) {
+                return;
+            }
+
+            speechSynthesisUtterance.text = messages[index];
+            window.speechSynthesis.speak(speechSynthesisUtterance);
+
+            speechSynthesisUtterance.onend = () => {
+                setTimeout(() => {
+                    speakMessages(index + 1);
+                }, 2000);
+            };
+        };
+
+        speakMessages(0);
     };
 
     const pause = () => {
--- frontend/src/scripts/contentScript.ts
+++ frontend/src/scripts/contentScript.ts
@@ -1,7 +1,9 @@
 // contentScript.ts
+console.log("Content script injected");
 const getImageUrls = () => {
   const images = document.querySelectorAll('img');
   const urls = Array.from(images).map(img => img.src);
+  console.log("HELLO TEST TEST")
   console.log("Extracted image URLs: ", urls);
   return urls;
 };
